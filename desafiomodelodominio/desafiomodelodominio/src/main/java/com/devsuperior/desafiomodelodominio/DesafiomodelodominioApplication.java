f2bd2e68067f010ab2dbdc36e90504548f8ce8ee
Eryck Barreto <eryck.barreto@hotmail.com>
2024-01-19 15:46:32 -0300

    estrutura de projeto criado

---
+++ desafiomodelodominio/desafiomodelodominio/src/main/java/com/devsuperior/desafiomodelodominio/DesafiomodelodominioApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiomodelodominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiomodelodominioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiomodelodominioApplication.class, args);
+	}
+
+}
---
+++ desafiomodelodominio/desafiomodelodominio/src/test/java/com/devsuperior/desafiomodelodominio/DesafiomodelodominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.desafiomodelodominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiomodelodominioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
