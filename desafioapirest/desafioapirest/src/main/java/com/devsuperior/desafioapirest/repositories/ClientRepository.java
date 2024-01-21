ea52585e5fbc38bf9eb3753e28722377d464bf7b
Eryck Barreto <eryck.barreto@hotmail.com>
2024-01-21 16:00:02 -0300

    criada a entity cliente e o repository client

---
+++ desafioapirest/desafioapirest/src/main/java/com/devsuperior/desafioapirest/repositories/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.devsuperior.desafioapirest.repositories;
+
+import com.devsuperior.desafioapirest.entities.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
