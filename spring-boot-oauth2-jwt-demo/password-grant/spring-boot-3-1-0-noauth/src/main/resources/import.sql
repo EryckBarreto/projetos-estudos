6357230ac28333169a385fca9a1b92dd86ad771a
Eryck Barreto <eryck.barreto@hotmail.com>
2024-01-26 20:28:07 -0300

    senhas salvas no formato bcrypt

--- spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
+++ spring-boot-oauth2-jwt-demo/password-grant/spring-boot-3-1-0-noauth/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$10$GJ9KK21fBh6zCyHYo5lZG.RE6xcuXwjhBPmv5zVlp05YtMc2VPWuC');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$10$GJ9KK21fBh6zCyHYo5lZG.RE6xcuXwjhBPmv5zVlp05YtMc2VPWuC');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
