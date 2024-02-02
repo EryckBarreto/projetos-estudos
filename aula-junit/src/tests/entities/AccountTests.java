2d7893495794616cf7be7702bd60c1051377e874
Eryck Barreto <eryck.barreto@hotmail.com>
2024-02-02 13:34:55 -0300

    refatorado testes com padrão factory

--- aula-junit/src/tests/entities/AccountTests.java
+++ aula-junit/src/tests/entities/AccountTests.java
@@ -3,6 +3,7 @@ package tests.entities;
 import entitites.Account;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 
@@ -11,12 +12,24 @@ public class AccountTests {
 
         double amount = 200.0;
         double expectedValue = 196.0;
-        Account acc = new Account(1L, 0.0);
+        Account acc = AccountFactory.createEmptyAccount();
 
         acc.deposit(amount);
 
         Assertions.assertEquals(expectedValue, acc.getBalance());
 
+    }
+
+    @Test
+    public void depositShouldDoNothingWhenNegativeAmount() {
+
+        double expectedValue = 100.0;
+        Account acc = AccountFactory.createAccount(expectedValue);
+        double amount = -200;
+
+        acc.deposit(amount);
+
+        Assertions.assertEquals(expectedValue, acc.getBalance());
     }
 
 }
