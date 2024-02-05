265b44587e18155401991860a54453a941148541
Eryck Barreto <eryck.barreto@hotmail.com>
2024-02-05 09:56:01 -0300

    testes unitários da aula finalizado

--- aula-junit/src/tests/entities/AccountTests.java
+++ aula-junit/src/tests/entities/AccountTests.java
@@ -32,4 +32,36 @@ public class AccountTests {
         Assertions.assertEquals(expectedValue, acc.getBalance());
     }
 
+    @Test
+    public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
+
+        double expectedValue = 0.0;
+        double initialBalance = 800.0;
+        Account acc = AccountFactory.createAccount(initialBalance);
+
+        double result = acc.fullWithdraw();
+
+        Assertions.assertTrue(expectedValue == acc.getBalance());
+        Assertions.assertTrue(result == initialBalance);
+
+    }
+
+    @Test
+    public void fullWithdrawShouldDecreaseBalanceWhenSufficientBalance() {
+
+        Account acc = AccountFactory.createAccount(800.0);
+        acc.withdraw(500.0);
+        Assertions.assertEquals(300.0, acc.getBalance());
+    }
+
+    @Test
+    public void withdrawShouldThrowExceptionWhenInsufficientBalance() {
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            Account acc = AccountFactory.createAccount(800.0);
+            acc.withdraw(801.0);
+        });
+
+    }
+
 }
